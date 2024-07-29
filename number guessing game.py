f3cfac115f24209bcba02ffe7fbc9bbfe3122462
tlpranathi <169073127+tlpranathi@users.noreply.github.com>
2024-07-29 11:49:09 +0530

    Add files via upload

---
+++ number guessing game.py
@@ -0,0 +1,44 @@
+def game():
+    print("$$$$#### NUMBER GUESSING GAME ####$$$$")
+    int1 = int(input("enter lower bound - "))
+    int2 = int(input("enter upper bound - "))
+    print("\nyou have only 7 chances to guess the number")
+
+    import random
+    mynum = random.randrange(int1, int2)
+
+    for i in range(1, 8):
+        guess = int(input("guess a number - "))
+        if guess != mynum:
+            if guess > mynum and guess < mynum+6:
+                print("your guess is a little higher than the number!!!")
+            elif guess < mynum and guess > mynum-6:
+                print("your guess is a little lower than the number!!!")
+            elif guess > mynum:
+                print("you guessed too high!!!")
+            elif guess < mynum:
+                print("you guessed too low!!!")
+
+
+        if guess == mynum:
+            print("\nCONGRATULATIONS")
+            if i == 1:
+                print("you guessed the number in", i, "chance" )
+            else:
+                print("you guessed the number in", i, "chances")
+            break
+
+    if guess != mynum:
+        print("\nYOU LOSE")
+        print("the number was", mynum)
+
+game()
+
+playagain = input("do you want to play again?\n yes/no - ")
+if playagain == "yes":
+    game()
+else:
+    pass
+
+
+
